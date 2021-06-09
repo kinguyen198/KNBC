457d533167e052d9c5e2a73a60a3d32c761372af
kinguyen198 <kinguyen166@gmail.com>
2021-06-09 17:38:39 +0700

    Update UI + Fix load chatScreen

--- src/Chats.js
+++ src/Chats.js
@@ -16,7 +16,7 @@ export default function ChatsScreen({navigation, route}) {
   const [chats, setChats] = useState([]);
   const url = 'http://127.0.0.1:5000';
   var socket = io(url);
-
+  
   const getUser = async () => {
     try {
       const jsonValue = await AsyncStorage.getItem('user');
@@ -33,46 +33,47 @@ export default function ChatsScreen({navigation, route}) {
     getUser();
     socket.connect();
     socket.on('incommingMessage', () => {
-      console.log('called');
+      console.log('called Chats');
       getMessage();
     });
   }, []);
   useEffect(() => {
     getMessages();
   }, [userId]);
+
   const getMessages = async () => {
     try {
       let response = await axios.get(url + '/chats/' + userId);
       if (response.status === 200) {
         let chats = [];
-        response.data.map(async active => {
-          if (active.sender === userId) {
-            await axios.get(url + '/find/' + active.reciever).then(res => {
-              if (active != null) {
+        for (var i = 0; i < response.data.length; i++) {
+          if (response.data[i].sender == userId) {
+            await axios
+              .get(url + '/find/' + response.data[i].reciever)
+              .then(res => {
                 const chatItem = {
-                  message: active.messages[0].text
-                    ? active.messages[0].text
+                  message: response.data[i].messages[0].text
+                    ? response.data[i].messages[0].text
                     : 'Sent an image',
                   user: res.data,
                 };
                 chats.push(chatItem);
-              }
-            });
+              });
           } else {
-            await axios.get(url + '/find/' + active.sender).then(res => {
-              if (active != null) {
+            await axios
+              .get(url + '/find/' + response.data[i].sender)
+              .then(res => {
                 const chatItem = {
-                  message: active.messages[0].text
-                    ? active.messages[0].text
+                  message: response.data[i].messages[0].text
+                    ? response.data[i].messages[0].text
                     : 'Sent an image',
                   user: res.data,
                 };
                 chats.push(chatItem);
-              }
-            });
+              });
           }
-          setChats(chats);
-        });
+        }
+        setChats(chats);
       }
     } catch (error) {
       console.error(error);
@@ -80,19 +81,10 @@ export default function ChatsScreen({navigation, route}) {
   };
   return (
     <View>
-      <Text
-        style={{
-          color: 'black',
-          fontSize: 36,
-          padding: '5%',
-          paddingLeft: '7%',
-          //fontFamily: 'Cairo-Regular',
-        }}>
-        Conversations
-      </Text>
       <View>
         <ScrollView
           style={{
+            paddingLeft: '5%',
             paddingHorizontal: '7%',
             marginBottom: '18%',
             paddingBottom: '1.5%',
@@ -110,15 +102,19 @@ export default function ChatsScreen({navigation, route}) {
               style={{
                 flex: 2,
                 flexDirection: 'row',
-                marginVertical: '2%',
-                paddingVertical: '4%',
+                //marginVertical: '2%',
+                paddingVertical: '5%',
                 borderRadius: 10,
               }}>
               <Image
                 source={{
                   uri: 'https://i.imgur.com/4vzW11a.png',
                 }}
-                style={{width: 60, height: 60, borderRadius: 70}}
+                style={{
+                  width: 60,
+                  height: 60,
+                  borderRadius: 70,
+                }}
               />
               <View
                 style={{
@@ -163,10 +159,23 @@ export default function ChatsScreen({navigation, route}) {
                   style={{
                     flex: 2,
                     flexDirection: 'row',
-                    marginVertical: '2%',
-                    paddingVertical: '4%',
+                    marginVertical: '1%',
                     borderRadius: 10,
+                    alignItems: 'center',
                   }}>
+                  <View
+                    style={{
+                      height: 1,
+                      backgroundColor: 'grey',
+                      opacity: 0.3,
+                      width: '100%',
+                      position: 'absolute',
+                      left: '21%',
+                      bottom: 0,
+                      alignSelf: 'flex-end',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}></View>
                   <Image
                     source={{
                       uri: chatItem.user.photo,
@@ -176,14 +185,14 @@ export default function ChatsScreen({navigation, route}) {
                   <View
                     style={{
                       flex: 2,
+                      width: '100%',
                       flexDirection: 'column',
-                      marginHorizontal: '5%',
-                      marginTop: '1%',
+                      marginHorizontal: '4%',
+                      marginBottom: '3%',
                     }}>
                     <Text
                       style={{
                         fontSize: 18,
-                        //fontFamily: 'Cairo-SemiBold',
                         color: 'black',
                       }}>
                       {chatItem.user.name}
@@ -192,7 +201,9 @@ export default function ChatsScreen({navigation, route}) {
                       style={{
                         fontSize: 14,
                         //fontFamily: 'Cairo-Light',
+                        opacity: 0.5,
                         color: 'black',
+                        marginTop: '3%',
                       }}>
                       {chatItem.message}
                     </Text>
